5fa2be711ed8cb83b83ed6fed58f14f2e291fbd3
Eliu <elcresan@gmail.com>
2016-09-10 13:36:39 +0200

    Add calculator

--- app/src/main/java/com/example/testing/testingexample/Calculator.java
+++ app/src/main/java/com/example/testing/testingexample/Calculator.java
@@ -1,5 +1,20 @@
 package com.example.testing.testingexample;
 
 public class Calculator {
+
+    public double sum(double a, double b){
+        return 0;
+    }
+
+    public double subtract(double a, double b){
+        return 0;
+    }
+
+    public double divide(double a, double b){
+        return 0;
+    }
+
+    public double multiply(double a, double b){
+        return 0;
+    }
 }
--
