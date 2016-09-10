3a07dbb8144655b1da7ccb2b94c42338e7b4a5e9
Eliu <elcresan@gmail.com>
2016-09-10 13:38:19 +0200

    Add testing class for calculator

---
+++ app/src/test/java/com/example/testing/testingexample/CalculatorTest.java
@@ -0,0 +1,32 @@
+package com.example.testing.testingexample;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testSum() throws Exception {
+
+    }
+
+    @Test
+    public void testSubtract() throws Exception {
+
+    }
+
+    @Test
+    public void testDivide() throws Exception {
+
+    }
+
+    @Test
+    public void testMultiply() throws Exception {
+
+    }
+}
